75931654281ec1ec89f334640b4538c8bf9b0d9b
RaziurC4S5E <105146023+RaziurC4S5E@users.noreply.github.com>
2025-10-29 22:46:13 +0600

    added the diagonal_winner() and check_diagonal() functions to check each diagonal to see if they match or not

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def assert_equal(actual, expected):
+def assert_equal(actual, expected): #function to check the result if actual matches with expected
 	if actual == expected:
 	    print("OK")
 	else:
 	    print(f"Error! {repr(actual)} != {repr(expected)}")
 		
-def row_winner(board):
+def row_winner(board): #row_winner function to check if any row matches either with X or O
     result = False
     for index in range(len(board)):
         result = each_row_check(board[index])
@@ -12,7 +12,7 @@ def row_winner(board):
             return result
     return result
 
-def each_row_check(row):
+def each_row_check(row): #function to check each row and return True if every value of the row matches or return False if it doesnt
     result = False
     for index in range(len(row)-1):
         if row[index] == '' or row[index] == " ":
@@ -24,7 +24,7 @@ def each_row_check(row):
                 return False
     return result
 
-def column_winner(board):
+def column_winner(board): #column_winner function to check if any column matches either with X or O
     result = False
     column = ""
     for r in range(len(board)):
@@ -36,7 +36,7 @@ def column_winner(board):
         column = ""
     return result
 
-def each_column_check(column):
+def each_column_check(column): #function to check each column and return True if every value of the column matches or return False if it doesnt
     result = False
     for index in range(len(column)-1):
         if column[index] == '' or column[index] == " ":
@@ -47,6 +47,8 @@ def each_column_check(column):
             else:
                 return False
     return result
+
+#below are some outputs to check if my code is correct or not using assert_equal() function
 
 assert_equal(
     row_winner(
@@ -92,4 +94,26 @@ assert_equal(
         ]
     ),
     True
+)
+
+assert_equal(
+    diagonal_winner(
+        [
+            ['O', 'X', 'O', 'X'],
+            [' ', 'O', 'X', ' '],
+            ['X', 'X', ' ', 'X'],
+            ['X', ' ', 'O', 'O']
+        ]
+    ),
+    True
+)
+assert_equal(
+    diagonal_winner(
+        [
+            ['X', 'X', ' '],
+            ['X', ' ', 'O'],
+            [' ', 'O', 'O']
+        ]
+    ),
+    False
 )
