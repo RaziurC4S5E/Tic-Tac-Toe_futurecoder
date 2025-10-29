8751f557796933f8f727d282a2d6e302e90c20d8
RaziurC4S5E <105146023+RaziurC4S5E@users.noreply.github.com>
2025-10-29 22:32:13 +0600

    added some comments for better understanding

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Tic-Tac-Toe_futurecoder
 Trying to build a Tic-Tac-Toe Game in Python based on instructions from futurecoder.io
+
+Each Tasks are decribed below:
+
+1. Write a function row_winner which returns True if board contains a winning row, i.e. a horizontal line which has the same character in all its entries (except ' ')
+
+2. write a similar function column_winner which checks for a winning column (a vertical line) of either piece
--- main.py
+++ main.py
@@ -3,6 +3,50 @@ def assert_equal(actual, expected):
 	    print("OK")
 	else:
 	    print(f"Error! {repr(actual)} != {repr(expected)}")
+		
+def row_winner(board):
+    result = False
+    for index in range(len(board)):
+        result = each_row_check(board[index])
+        if result == True:
+            return result
+    return result
+
+def each_row_check(row):
+    result = False
+    for index in range(len(row)-1):
+        if row[index] == '' or row[index] == " ":
+            return False
+        else:
+            if row[index] == row[index+1]:
+                result = True
+            else:
+                return False
+    return result
+
+def column_winner(board):
+    result = False
+    column = ""
+    for r in range(len(board)):
+        for c in range(len(board)):
+            column += board[c][r]
+        result = each_column_check(column)
+        if result == True:
+            return result
+        column = ""
+    return result
+
+def each_column_check(column):
+    result = False
+    for index in range(len(column)-1):
+        if column[index] == '' or column[index] == " ":
+            return False
+        else:
+            if column[index] == column[index+1]:
+                result = True
+            else:
+                return False
+    return result
 
 assert_equal(
     row_winner(
@@ -22,6 +66,29 @@ assert_equal(
             ['X', ' ', 'X'],
             ['O', 'X', 'X'],
             ['O', 'O', 'O']
+        ]
+    ),
+    True
+)
+
+assert_equal(
+    column_winner(
+        [
+            ['X', 'O', ' '],
+            ['X', 'O', ' '],
+            ['O', 'X', ' ']
+        ]
+    ),
+    False
+)
+
+assert_equal(
+    column_winner(
+        [
+            ['X', 'O', ' ', 'X'],
+            [' ', 'O', 'X', 'O'],
+            ['O', 'O', 'X', 'X'],
+            ['O', 'O', 'X', ' ']
         ]
     ),
     True
